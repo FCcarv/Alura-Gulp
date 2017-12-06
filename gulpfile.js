e6d77a5f7048aae7d6dfeafbadb4beb8295aacbb
francisco <kiko.fccarvalho@gmail.com>
2017-12-06 08:38:22 -0200

    Substitui 2 task 'build-js' e 'build-html' por uma mais completa task 'usemin'

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,22 @@ var gulp = require('gulp')
 ,clean = require('gulp-clean')
 ,concat = require('gulp-concat')
 ,htmlReplace = require('gulp-html-replace')
-,uglify = require('gulp-uglify');
+,uglify = require('gulp-uglify')
+,usemin = require('gulp-usemin')
+,cssmin = require('gulp-cssmin');
 
 /*esta a tarefa que esta dando o start e faz com que todas as 3 buid-img, 
 buid-js*,buid-html e funciona juntas em paralelo**/
+// gulp.task('default',['copy'], function() {
+//     gulp.start('build-img','build-js','build-html');
+// });
+
+
+
+/*foi modificada esta tarefa que esta dando o start e faz com que todas as 2 buid-img, 
+usemin e funciona juntas em paralelo*/
 gulp.task('default',['copy'], function() {
-    gulp.start('build-img','build-js','build-html');
+    gulp.start('build-img','usemin');
 });
 
 /*roda depois da tarefa clean e copia todas as pastas e os arquivos, p pasta dist*/
@@ -31,23 +41,38 @@ gulp.task('build-img', function() {
 	.pipe(gulp.dest('dist/img/'));
 });
 
-/*junta e concatena todos os arquivos js pra dentro da pasta dist na ordem abaixo :
-1-jquery.js', 2-dist/js/home.js',3-dist/js/produto.js */
-gulp.task('build-js', function() {
+// /*junta e concatena todos os arquivos js pra dentro da pasta dist na ordem abaixo :
+// 1-jquery.js', 2-dist/js/home.js',3-dist/js/produto.js */
+// gulp.task('build-js', function() {
 
-    gulp.src(['dist/js/jquery.js', 'dist/js/home.js','dist/js/produto.js'])
-    .pipe(concat('all.js'))
-    .pipe(uglify()) //minifica o arquivo all.js antes de mandar pra pasta dist
-    .pipe(gulp.dest('dist/js'));
-});
+//     gulp.src(['dist/js/jquery.js', 'dist/js/home.js','dist/js/produto.js'])
+//     .pipe(concat('all.js'))
+//     .pipe(uglify()) //minifica o arquivo all.js antes de mandar pra pasta dist
+//     .pipe(gulp.dest('dist/js'));
+// });
 
-/*modifica os nome dos aquivos js na chamadas das paginas html,
-indenticadas pelo comentario build:js */
-gulp.task('build-html', function() {
+// /*modifica os nome dos aquivos js na chamadas das paginas html,
+// indenticadas pelo comentario build:js */
+// gulp.task('build-html', function() {
+    
+//     gulp.src('dist/**/*.html')
+//     .pipe(htmlReplace({
+//     	js:'js/all.js'
+//     }))
+//     .pipe(gulp.dest('dist'));
+// });
+
+
+/*Essa tarefa substitui outras duas 'build-js','build-html',*/
+/*modifica os nome dos aquivos js na chamadas das paginas html, 
+indenticadas pelo comentario build:js e tbm arquivos css build:css */
+/*E ainda minifica os arquivos css e js*/
+gulp.task('usemin', function() {
     
     gulp.src('dist/**/*.html')
-    .pipe(htmlReplace({
-    	js:'js/all.js'
-    }))
-    .pipe(gulp.dest('dist'));
+    	.pipe(usemin({
+    		'js':[uglify],
+    		'css':[cssmin]
+    	}))
+    	.pipe(gulp.dest('dist'));
 });
